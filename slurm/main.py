806573d512b738a44aa76c3a22b9c4c46c0a5623
Rhythmicc <rhythmlian.cn@gmail.com>
2023-07-09 15:37:33 +0800

    update

--- slurm/main.py
+++ slurm/main.py
@@ -25,7 +25,7 @@ def cancel(job_id: str):
 
 
 @app.command()
-def gen_sbatch_template(name: str):
+def template(name: str):
     """
     生成sbatch模板
 
@@ -55,11 +55,12 @@ def get_job_id(command_output: str):
     """
     return command_output.split()[-1].strip()
 
-def view_log(log_path: str):
+def view_log(log_path: str, job_id: str):
     """
     查看日志
 
     :param log_path: 日志路径
+    :param job_id: 任务ID
     """
     # 实时显示日志，并在任务结束后退出
     from rich.markdown import Markdown
@@ -67,8 +68,9 @@ def view_log(log_path: str):
     from threading import Thread
 
     def my_print(line):
+        line = line.strip()
         if line.startswith("__START__"):
-                QproDefaultStatus(line.replace("__START__", "")).start()
+            QproDefaultStatus(line.replace("__START__", "")).start()
         elif line.startswith("__STOP__"):
             QproDefaultStatus.stop()
         elif line.startswith("__SPLIT__"):
@@ -88,23 +90,33 @@ def view_log(log_path: str):
             error.append(line)
         proc.stderr.close()
     
+    def _monitor_job_running(proc, job_id):
+        import time
+        while True:
+            _, ct = external_exec(f'squeue -j {job_id}', without_output=True)
+            if not ct.split('\n')[1:]:
+                break
+            time.sleep(1)
+        proc.kill()
+    
     output = []
     error = []
     proc = Popen(['tail', '-f', log_path], stdout=PIPE, stderr=PIPE)
     Thread(target=_output_reader, args=(proc, output)).start()
     Thread(target=_error_reader, args=(proc, error)).start()
+    Thread(target=_monitor_job_running, args=(proc, job_id)).start()
     while proc.poll() is None:
         if output:
-            print(output.pop().decode('utf-8'), end='')
+            my_print(output.pop().decode('utf-8'))
         if error:
-            print(error.pop().decode('utf-8'), end='')
+            my_print(error.pop().decode('utf-8'))
     
     # 任务结束后，显示最后的日志
     while output:
-        print(output.pop().decode('utf-8'), end='')
+        my_print(output.pop().decode('utf-8'))
     while error:
-        print(error.pop().decode('utf-8'), end='')
-    print('任务结束！')
+        my_print(error.pop().decode('utf-8'))
+    QproDefaultConsole.print(QproInfoString, f'任务已结束，日志文件: {log_path}')
 
 
 @app.command()
@@ -116,37 +128,39 @@ def submit(script_path: str):
     """
     import time
 
-    _, ct = external_exec('sbatch {}'.format(script_path))
+    _, ct = external_exec('sbatch {}'.format(script_path), without_output=True)
     job_id = get_job_id(ct)
+    QproDefaultConsole.print(QproInfoString, f'任务已提交，任务ID: {job_id}')
+    QproDefaultStatus('正在等待日志文件生成...').start()
     while not os.path.exists(f'log/{job_id}.log'):
         time.sleep(0.1)
-    
-    view_log(f'log/{job_id}.log')
+    QproDefaultStatus.stop()
+    view_log(f'log/{job_id}.log', job_id)
 
 
 @app.command()
 def top():
     """
     查看任务状态
-
-    使用squeue命令
     """
     import time
     from rich.live import Live
+    from rich.align import Align
     from QuickStart_Rhy.TuiTools.Table import qs_default_table
     table = qs_default_table(
         ['任务ID', '任务队列', '任务名称', '用户', '状态', '用时', '节点数目', '节点列表'], title='任务队列\n')
 
-    with Live(console=QproDefaultConsole, refresh_per_second=1) as live:
+    with Live(console=QproDefaultConsole, auto_refresh=False) as live:
         while True:
-            _, ct = external_exec('squeue')
+            _, ct = external_exec('squeue', without_output=True)
             table = qs_default_table(['任务ID', '任务队列', '任务名称', '用户', '状态', '用时', '节点数目', '节点列表'], title='任务队列\n')
             for line in ct.split('\n')[1:]:
                 if line.strip() == '':
                     continue
                 line = line.split()
                 table.add_row(*line[:8])
-            live.update(table)
+            live.update(Align.center(table))
+            live.refresh()
             time.sleep(1)
 
 
